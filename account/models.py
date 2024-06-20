cd3de6e2d9cf280c90ff400b02df051fe679ebd6
Alisherbek2001 <alisherbekyuldashev1211@gmail.com>
2024-06-20 10:09:42 +0500

    initial commit accoount and common

---
+++ account/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("first name"), max_length=255)
+    last_name = models.CharField(_("last name"), max_length=255)
+    email = models.EmailField(_("email adress"), unique=True)
+    phone = models.CharField(_("phone number"), max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_partner = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='User_Image/',null=True,blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
